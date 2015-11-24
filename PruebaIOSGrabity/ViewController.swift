e6b98a6674117617de5c984a1a1be542afa38a33
padazab <padbem@gmail.com>
2015-11-24 06:21:57 -0500

    Initial Commit

---
+++ PruebaIOSGrabity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaIOSGrabity
+//
+//  Created by Pedro Alonso Daza Balaguera on 24/11/15.
+//  Copyright © 2015 Pedro Alonso Daza Balaguera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
